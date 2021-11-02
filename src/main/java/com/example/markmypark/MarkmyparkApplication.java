df98f79ad99a4462a133ea96e117353a6679638a
VaishnaviShri <vaishnavishri2019@gmail.com>
2021-11-03 03:06:41 +0530

    Initial commit

---
+++ src/main/java/com/example/markmypark/MarkmyparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.markmypark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkmyparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkmyparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/markmypark/MarkmyparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markmypark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkmyparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
