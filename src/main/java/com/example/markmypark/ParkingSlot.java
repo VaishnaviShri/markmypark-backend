465b50e2051f2b020962fded17eb1e16b296c5f2
VaishnaviShri <vaishnavishri2019@gmail.com>
2021-12-03 17:56:07 +0530

    Fix worker id refernce

--- src/main/java/com/example/markmypark/ParkingSlot.java
+++ src/main/java/com/example/markmypark/ParkingSlot.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gcp.data.firestore.Document;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 @Document(collectionName = "parkingslots")
 public class ParkingSlot {
@@ -13,7 +14,7 @@ public class ParkingSlot {
     @DocumentId
     public String id;
     public String location;
-    public Mono<Worker> worker;
+    public Worker worker = null;
     public double parkingRatePerHour;
     public double totalPrice;
     public ArrayList<DayBooking> allBookings;
@@ -22,9 +23,9 @@ public class ParkingSlot {
         return totalPrice;
     }
 
-   // WorkerRepository workerRepository;
+    WorkerService workerService;
     public void setTotalPrice() {
-        this.totalPrice = parkingRatePerHour + Objects.requireNonNull(worker.block()).getRatePerHour();
+        this.totalPrice = parkingRatePerHour + Objects.requireNonNull(worker).getRatePerHour();
     }
 
 
@@ -32,75 +33,12 @@ public class ParkingSlot {
 
     }
 
-    public ParkingSlot(String location, String workerID, double parkingRatePerHour) {
+    public ParkingSlot(String location, String workerID, double parkingRatePerHour) throws ExecutionException, InterruptedException {
         this.location = location;
-      //  this.worker = workerRepository.findById(workerID);
+       this.worker =workerService.getWorker(workerID);
         this.parkingRatePerHour = parkingRatePerHour;
     }
 
     //if no match found display based on location
-/*
 
-    public ArrayList<ParkingSlot> findbyloc(String loc, ArrayList<ParkingSlot> arr) {
-        ArrayList<ParkingSlot> match = new ArrayList<>();
-        for(ParkingSlot obj : arr){
-            if(obj.location != null && obj.location.equals(loc))
-                match.add(obj);
-        }
-
-        return match;
-    }
-
-    public ArrayList<ParkingSlot> find(String loc, Date dt, int ch_in, int ch_out, ArrayList<ParkingSlot> arr) {
-        ArrayList<ParkingSlot> match = new ArrayList<>();
-        for(ParkingSlot obj : arr){
-            if(obj.location != null && obj.location.equals(loc)){
-                for(DayBooking db : obj.allBookings){
-                    if(db.date.equals(dt)){
-                        int flag = 0;
-                        for(int i = ch_in; i <= ch_out; i++){
-                            if(db.usersList.get(i) != null){
-                                flag = 1;
-                                break;
-                            }
-                        }
-                        if(flag == 0)
-                            match.add(obj);
-                    }
-                }
-            }
-        }
-
-        return match;
-    }
-
-    public ArrayList<ParkingSlot> find_by_date(Date dt, ArrayList<ParkingSlot> arr) {
-        ArrayList<ParkingSlot> match = new ArrayList<ParkingSlot>();
-        for(ParkingSlot obj : arr){
-            if(obj.date != null && obj.date.equals(dt))
-                match.add(obj);
-        }
-
-        return match;
-    }
-
-    public ArrayList<ParkingSlot> find_by_checkin(Date ch_in, ArrayList<ParkingSlot> arr) {
-        ArrayList<ParkingSlot> match = new ArrayList<ParkingSlot>();
-        for(ParkingSlot obj : arr){
-            if(obj.check_in != null && obj.check_in.equals(ch_in))
-                match.add(obj);
-        }
-
-        return match;
-    }
-
-    public ArrayList<ParkingSlot> find_by_checkout(Date ch_out, ArrayList<ParkingSlot> arr) {
-        ArrayList<ParkingSlot> match = new ArrayList<ParkingSlot>();
-        for(ParkingSlot obj : arr){
-            if(obj.check_out != null && obj.check_out.equals(ch_out))
-                match.add(obj);
-        }
-
-        return match;
-    }*/
 }
