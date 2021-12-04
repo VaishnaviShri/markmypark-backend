fd7e0e2c0e39e8c567e41f23fdf75021bcf64422
VaishnaviShri <vaishnavishri2019@gmail.com>
2021-12-04 15:22:39 +0530

    hello world

--- src/main/java/com/example/markmypark/MarkmyparkApplication.java
+++ src/main/java/com/example/markmypark/MarkmyparkApplication.java
@@ -2,6 +2,8 @@ package com.example.markmypark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MarkmyparkApplication {
@@ -10,4 +12,11 @@ public class MarkmyparkApplication {
 		SpringApplication.run(MarkmyparkApplication.class, args);
 	}
 
+}
+@RestController
+class HelloController{
+	@GetMapping("/")
+	String hello(){
+		return "hello vaish";
+	}
 }
