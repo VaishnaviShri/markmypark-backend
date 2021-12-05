f54e781a3f28b75369b6be608de5a8589413d1e3
arjun <arjun.muthiah@yahoo.in>
2021-12-05 16:16:58 +0530

    changed more code to use date as string and check in and check out as integers, added no argument constructor to booking class file, added the call to ParkingSlotService's addParkingSlotBooking method in the addUserBooking method of UserService.java, edited the booking logic to make it complete, changed DayBooking to contain List of Strings instead of a map with non-string key and edited functions to work with the updated DayBooking class

--- src/main/java/com/example/markmypark/entites/DayBooking.java
+++ src/main/java/com/example/markmypark/entites/DayBooking.java
@@ -4,9 +4,22 @@ import java.util.*;
 
 public class DayBooking {
     public String date;
-    public Map<Integer, String> usersList = new TreeMap<Integer, String>();//map with key being hour and value being user id of the user booked for that hour or null if the hour is free
+    //public Map<Integer, String> usersList = new TreeMap<Integer, String>();//map with key being hour and value being user id of the user booked for that hour or null if the hour is free
+    public List <String> usersList = new ArrayList<>();
+
+    public String getUsersListHour(int h) { return usersList.get(h - 10); }
 
     public void setHour(int h, String userID) {
-        this.usersList.put(h, userID);
+        this.usersList.set(h - 10, userID);
+    }
+
+    public DayBooking() {
+        for(int i = 10; i <= 17; i++)
+            this.usersList.add(i - 10, null);
+    }
+
+    public DayBooking(String dt) {
+        this();
+        this.date = dt;
     }
 }
